32545882143f073d03c410576206aad97b789aff
jahanger2106 <117963781+jahanger2106@users.noreply.github.com>
2022-11-11 07:16:08 +0300

    Add files via upload

---
+++ jahanger 2-ameliy.py
@@ -0,0 +1,16 @@
+Python 3.10.7 (tags/v3.10.7:6cc6b13, Sep  5 2022, 14:08:36) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+============== RESTART: C:/Users/USER/Desktop/Jahanger 1-ameliy.py =============
+birinshi sandi kiritin: 1
+ekinshi sandi kiritin: 2
+a sani ten b ga 2
+b sani ten a ga 1
+>>> 
+============== RESTART: C:/Users/USER/Desktop/Jahanger 1-ameliy.py =============
+putin san kiritin: 1
+putin san kiritin: 12
+putin san kiritin: 34
+a saninin aqirgi cifri 1
+b saninin aqirgi cifri 2
+c saninin aqirgi cifri 4
